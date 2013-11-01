92dec0dcc7176c3c071e49f2336de272023af4c8
Eyal Toledano <etoledano@playigl.com>
2013-11-01 15:36:50 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Esportsdirectory::Application.config.secret_token = '9f4b978d50bfc1a90eaa107b4b6d471129a9d275c52eb0ae83b27f499399027facc670cb2c817498d97dec939173aef97bb86978a7f364c6449e9050d150dc95'
